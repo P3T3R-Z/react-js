999084b917dbb943c6b641c0663d5dbcb69fe235
p3 <40552367+P3T3R-Z@users.noreply.github.com>
2019-03-06 14:28:16 +0800

    '增加react-loadable'

--- src/App.js
+++ src/App.js
@@ -3,12 +3,38 @@ import "./assets/sass/base.css";
 
 import { HashRouter, Route } from "react-router-dom";
 import Header from "./header";
-import Home from "./home";
-import Detail from "./detail/detail";
+//import Home from "./home";
+//import Detail from "./detail/detail";
 
 //方便redux与react协作插件,使用该插件后不需要使用store.getState, store.subscribe等api
 import { Provider } from "react-redux";
 import store from "./store";
+
+import Loadable from "react-loadable";
+
+const MyLoadingComponent = props => {
+  if (props.isLoading) {
+      return <div>Loading...</div>
+  }
+  else if (props.error) {
+      return <div>Error! <button onClick={ props.retry }>重载</button></div>
+  }
+  else {
+      return null;
+  }
+};
+
+const AsyncHome = Loadable({
+  loader: () => import('./home'),
+  loading: MyLoadingComponent,
+});
+const AsyncDetail = Loadable({
+  loader: () => import('./detail/detail'),
+  loading: MyLoadingComponent,
+});
+
+
+
 class App extends PureComponent {
   render() {
     return (
@@ -17,8 +43,8 @@ class App extends PureComponent {
           <HashRouter>
             <Fragment>
               <Header />
-              <Route path="/" component={Home} exact />
-              <Route path="/detail" component={Detail} />
+              <Route path="/" component={AsyncHome} exact />
+              <Route path="/detail" component={AsyncDetail} />
             </Fragment>
           </HashRouter>
         </Provider>
